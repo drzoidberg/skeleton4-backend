d5575c60bf7249d746d6449a78319178cb26966a
Jorge Vicens <gromit.jvv@gmail.com>
2020-09-28 13:33:30 +0200

    fixing manualCorsMiddleware

--- server/middlewares/manualCors.middleware.js
+++ server/middlewares/manualCors.middleware.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '../../.env' });
 
 module.exports = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', `http://${process.env.HOSTNAME}:${process.env.FRONTEND_PORT}`);
+    res.setHeader('Access-Control-Allow-Origin', `http://${process.env.HOSTNAME || 'localhost'}:${process.env.FRONTEND_PORT || '5000'}`);
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
