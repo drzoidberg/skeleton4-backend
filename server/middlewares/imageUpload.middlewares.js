ded2436787e1014ec8fe764ba93fe96e6da23a92
Jorge Vicens <gromit.jvv@gmail.com>
2020-09-30 07:44:39 +0200

    commented imageUpload.middleware

--- server/middlewares/imageUpload.middlewares.js
+++ server/middlewares/imageUpload.middlewares.js
@@ -7,18 +7,19 @@ const MIME_TYPE_MAP = {
     'image/jpg': 'jpg',
 };
 
+/* configuring multer instance for avatar uploads */
 const imageUpload = multer({
     limits: 500000,
     storage: multer.diskStorage({
-        destination: (req, file, cb) => {
+        destination: (req, file, cb) => {                                   /* setting file path. Path must exist */
             cb(null, 'uploads/images');
         },
         filename: (req, file, cb) => {
-            const ext = MIME_TYPE_MAP[file.mimetype];
-            cb(null, uuid() + '.' + ext);
+            const ext = MIME_TYPE_MAP[file.mimetype];                       /* setting filename extension */
+            cb(null, uuid() + '.' + ext);                                   /* generating unique filename using uuid library */
         },
         fileFilter: (req, file, cb) => {
-            const isValid = !!MIME_TYPE_MAP[file.mimetype];
+            const isValid = !!MIME_TYPE_MAP[file.mimetype];                 /* trick to converting a match into a truthy/falsy value */
             let error = isvalid ? null : new Error('invalid mime type')
             cb(error, isValid);
         }
