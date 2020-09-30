b3eb5e159aa9c02f43a82ba2787933fc55582d80
Jorge Vicens <gromit.jvv@gmail.com>
2020-09-30 08:12:13 +0200

    commented server

--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ const config = require('../config/config');
 const app = require('./express');
 const mongoose = require('mongoose');
 
+/* configuring db connection */
 mongoose.connect(config.mongoUri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
 });
 
+/* handling db connection error */
 mongoose.connection.on('error', () => {
     throw new Error(`Unable to connect to database: ${config.mongoUri}`);
 });
 
+/* launching the server */
 app.listen(config.port, (error) => {
     if (error) {
         console.log(error);
