d61667c8565fcba78f873102876f3f825a6dad77
Jorge Vicens <gromit.jvv@gmail.com>
2020-09-30 07:49:06 +0200

    commented manualCors.middleware

--- server/middlewares/manualCors.middleware.js
+++ server/middlewares/manualCors.middleware.js
@@ -1,10 +1,11 @@
 require('dotenv').config({ path: '../../.env' });
 
+// setting http headers for allowing only a different origin: the one specified in the .env file
 module.exports = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', `http://${process.env.HOSTNAME || 'localhost'}:${process.env.FRONTEND_PORT || '5000'}`);
     res.setHeader(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        'X-Requested-With, Content-Type, Accept, Authorization'
     );
     res.setHeader(
         'Access-Control-Allow-Credentials',
