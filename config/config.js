c8fa3ac578727cfb4d64edcb706de478cc7807d1
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-10-05 09:02:18 +0200

    updated config env file

--- config/config.js
+++ config/config.js
@@ -2,9 +2,15 @@ require('dotenv').config({ path: '../.env' });
 
 const config = {
     env: process.env.NODE_ENV || 'development',
+    googleClient: process.env.GOOGLE_CLIENT || 'fill_the_dotenv_field',
+    jwtAccountActivation: process.env.JWT_ACCOUNT_ACTIVATION || 'fill_the_dotenv_field',
+    jwtResetPassword: process.env.JWT_RESET_PASSWORD || 'fill_the_dotenv_field',
+    jwtSecret: process.env.JWT_SECRET || 'fill_the_dotenv_field',
     port: process.env.PROJECT_PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'ultraMegaSecretKey',
     projectTitle: process.env.PROJECT_NAME || 'MERN-skeleton2',
+    sendgridApiKey: process.env.SENDGRID_API_KEY || 'fill_the_dotenv_field',
+    sendgridEmailFrom: process.env.SENDGRID_EMAIL_FROM || 'fill_the_dotenv_field',
+    sendgridEmailTo: process.env.SENDGRID_EMAIL_TO || 'fill_the_dotenv_field',
                                 /* apply custom project clientUrl only if
                                     ALL .env fields are filled,
                                     otherwise use a default clientUrl */
