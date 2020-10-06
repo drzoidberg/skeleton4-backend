c3e246e306cd959cd50131cbea253ae9086e8ec5
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-10-06 09:47:38 +0200

    removed .old directory

--- server/.old/controllers/users/read.controller.js
+++
@@ -1,9 +0,0 @@
-// read user
-module.exports = (req, res, next) => {
-
-    // resetting hashed_password & salt for security purposes
-    req.profile.hashed_password = undefined;
-    req.profile.salt = undefined;
-
-    return res.json(req.profile);
-};
